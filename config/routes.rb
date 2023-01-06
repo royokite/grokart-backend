e985a28cb7964cc3e3f3091bb839d7f08500ac97
RoyOkite <royokite@gmail.com>
2023-01-06 21:54:51 +0300

    Models, Controllers, Serializers and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :deliveries
+  resources :payments
+  resources :transactions
+  resources :categories
+  resources :orders
+  resources :carts
+  resources :products
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
