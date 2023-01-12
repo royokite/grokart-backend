4d77b61cf7e507431d50f741bc6678b74cb8a645
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2023-01-12 08:43:56 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   resources :users
 
   post 'login', to: 'authentication#login'
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  get 'view_order' => 'cart#view_order'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
