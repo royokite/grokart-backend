6757934e6943bce97e4c0cdf5aa7522ba773e531
jaime <jamesowiti24@gmail.com>
2025-05-07 14:57:10 +0300

    Improved jwt token

--- app/controllers/concerns/jwt_token.rb
+++ app/controllers/concerns/jwt_token.rb
@@ -1,17 +1,19 @@
 require "jwt"
 
 module JwtToken
+  extend ActiveSupport::Concern
 
-    extend ActiveSupport::Concern
-    SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  SECRET_KEY = Rails.application.credentials.secret_key_base
 
-    def jwt_encode(payload, exp: 7.days.from_now)
-        payload[:exp] = exp.to_i
-        JWT.encode(payload, SECRET_KEY)
-    end
+  def jwt_encode(payload, exp: 7.days.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
 
-    def jwt_decode(token)
-        decoded = JWT.decode(token, SECRET_KEY)[0]
-        HashWithIndifferentAccess.new decoded
-    end
+  def jwt_decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new(decoded)
+  rescue JWT::DecodeError, JWT::ExpiredSignature, JWT::VerificationError => e
+    raise e  # Let ApplicationController handle this
+  end
 end
